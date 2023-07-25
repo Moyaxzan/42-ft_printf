cbb24c866a6be2655ef772891214b36b300561b0
Tao SAINT PAUL AMOURDAM <tsaint-p@paul-f4Br5s2.clusters.42paris.fr>
2023-07-25 12:35:23 +0200

    fix program according to 42 printf

--- main.c
+++ main.c
@@ -2,65 +2,66 @@
 #include "./include/ft_printf.h"
 #define INT_MAX 2147483647
 #define INT_MIN -2147483648
-#define LONG_MAX 2147483647
-#define LONG_MIN -2147483648
 #define ULONG_MAX 4294967295
 
 int main(void)
 {
-	// char *ptr = "hidden";
-	// //ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
-	// printf("\n%%s\n");
-	// printf("size printf = %d\n", printf("%s ", ptr));
-	// printf("size printf = %d\n", ft_printf("%s ", ptr));
-	// printf("%d ", printf("%s ", (char *)NULL));
-	// fflush(stdout);
-	// printf("%d\n", ft_printf("%s ", NULL));
+	char *ptr = "hidden";
+	//ssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+	printf("%d\n", printf("|o = %s|\n", ""));
+	fflush(stdout);
+	ft_printf("%d\n", ft_printf("|o = %s|\n", ""));
+	printf("\n%%s\n");
+	printf("size printf = %d\n", printf("%s ", ptr));
+	printf("size printf = %d\n", ft_printf("%s ", ptr));
+	printf("%d ", printf("%s ", (char *)NULL));
+	fflush(stdout);
+	printf("%d\n", ft_printf("%s ", NULL));
 
-	// //dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
-	// printf("\n%%d\n");
-	// printf("||%d  ", 123456789);
-	// fflush(stdout);
-	// ft_printf("%d||\n", 123456789);
-	// printf("||%d  ", 	-2147483647);
-	// fflush(stdout);
-	// ft_printf("%d||\n", -2147483647);
-	// fflush(stdout);
-	// ft_printf("size = %d", printf("||%d ", -1));
-	// fflush(stdout);
-	// ft_printf("size = %d\n", ft_printf("%d|| ", -1));
+	// dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd
+	printf("\n%%d\n");
+	printf("||%d  ", 123456789);
+	fflush(stdout);
+	ft_printf("%d||\n", 123456789);
+	printf("||%d  ", 	-2147483647);
+	fflush(stdout);
+	ft_printf("%d||\n", -2147483647);
+	fflush(stdout);
+	ft_printf("size = %d", printf("||%d ", -1));
+	fflush(stdout);
+	ft_printf("size = %d\n", ft_printf("%d|| ", -1));
 
-	// // ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
-	// printf("\n%%p\n");
-	// printf("%p  ", ptr);
-	// fflush(stdout);
-	// ft_printf("%p\n", ptr);
-	// printf("%d ", printf("%p  ", NULL));
-	// fflush(stdout);
-	// printf("%d\n", ft_printf("%p  ", NULL));
+	// ppppppppppppppppppppppppppppppppppppppppppppppppppppppppppp
+	printf("\n%%p\n");
+	printf("%p  ", ptr);
+	fflush(stdout);
+	ft_printf("%p\n", ptr);
+	printf("%d ", printf("%p  ", NULL));
+	fflush(stdout);
+	printf("%d\n", ft_printf("%p  ", NULL));
 
-	// // uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
-	// printf("\n%%u\n");
-	// ft_printf("%d ", printf("%u ", -123456789));
-	// fflush(stdout);
-	// ft_printf("%d\n", ft_printf("%u ", -123456789));
-	// printf("%u ", 4294967295);
-	// fflush(stdout);
-	// ft_printf("%u\n", 4294967295);
-	// printf("%u ", 0);
-	// fflush(stdout);
-	// ft_printf("%u\n", 0);
+	// uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
+	printf("\n%%u\n");
+	ft_printf("%d ", printf("%u ", -123456789));
+	fflush(stdout);
+	ft_printf("%d\n", ft_printf("%u ", -123456789));
+	printf("%u ", 4294967295);
+	fflush(stdout);
+	ft_printf("%u\n", 4294967295);
+	printf("%u ", 0);
+	fflush(stdout);
+	ft_printf("%u\n", 0);
 
-	// // xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-	// printf("\n%%x\n");
-	// printf("%x\n",9223372036854775807LL);
-	// ft_printf("%x\n",9223372036854775807LL);
-	// ft_printf("size = %d", printf("real %x",4294967296));
-	// fflush(stdout);
-	// ft_printf("size = %d\n", ft_printf("mine %x",4294967296));
-	// printf(" %d %d %d %d %d %d %d\n", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
-	// fflush(stdout);
-	// ft_printf(" %d %d %d %d %d %d %d", INT_MAX, INT_MIN, LONG_MAX, LONG_MIN, ULONG_MAX, 0, -42);
+	// xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+	printf("\n%%x\n");
+	printf("%x\n",9223372036854775807LL);
+	ft_printf("%x\n",9223372036854775807LL);
+	ft_printf("size = %d", printf("real %x",4294967296));
+	fflush(stdout);
+	ft_printf("size = %d\n", ft_printf("mine %x",4294967296));
+	printf(" %d %d %d %d %d\n", INT_MAX, INT_MIN, ULONG_MAX, 0, -42);
+	fflush(stdout);
+	ft_printf(" %d %d %d %d %d\n", INT_MAX, INT_MIN, ULONG_MAX, 0, -42);
 	printf("%d\n", printf("%kdaflkjhasldkfj\n"));
 	printf("%d\n", printf("ilosuayhdg%"));
 	fflush(stdout);
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 12:16:28 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/07/24 14:28:50 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/07/25 12:29:32 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,49 @@
 
 int	write_conversion(const char *format, va_list args)
 {
-	if (!(*format))
-		return (0);
-	if (*format == '%')
+	if (!(format[1]))
+		return (-1);
+	if (format[1] == '%')
 		return (write(1, "%", 1));
-	if (*format == 'c')
+	if (format[1] == 'c')
 		return (ft_putchar((char) va_arg(args, int)));
-	if (*format == 's')
+	if (format[1] == 's')
 		return (ft_putstr((char *) va_arg(args, char *)));
-	if (*format == 'd' || *format == 'i')
+	if (format[1] == 'd' || format[1] == 'i')
 		return (putnbr_decimal(va_arg(args, int)));
-	if (*format == 'x')
+	if (format[1] == 'x')
 		return (print_hex(va_arg(args, unsigned long), BASE_L16));
-	if (*format == 'X')
+	if (format[1] == 'X')
 		return (print_hex(va_arg(args, unsigned long), BASE_U16));
-	if (*format == 'p')
+	if (format[1] == 'p')
 		return (print_pointer(va_arg(args, unsigned long)));
-	if (*format == 'u')
+	if (format[1] == 'u')
 		return (print_unsigned(va_arg(args, unsigned int)));
-	return (write(1, format, 1));
+	return (write(1, format, 2));
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
 	int		nb_char_written;
+	int		conv;
 
 	if (!format)
 		return (-1);
 	va_start(args, format);
 	nb_char_written = 0;
+	conv = 0;
 	while (*format)
 	{
 		if (*format == '%')
-			nb_char_written += write_conversion(++format, args);
-		else
 		{
-			write(1, format, 1);
-			nb_char_written++;
+			conv = write_conversion(format++, args);
+			if (conv == -1)
+				return (-1);
+			nb_char_written += conv;
 		}
+		else
+			nb_char_written += write(1, format, 1);
 		if (*format)
 			format++;
 	}
